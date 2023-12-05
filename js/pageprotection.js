6cb935924d6643c85b8207ba2ce6618fa63f4edc
Rai Pramana <rai.pramana46@gmail.com>
2023-12-05 10:18:19 +0800

    modified:   js/pageprotection.js

--- js/pageprotection.js
+++ js/pageprotection.js
@@ -1,11 +1,11 @@
 checkLogin().then((isLoggedIn) => {
     if (!isLoggedIn) {
-        if (!window.location.pathname.endsWith("index.html") || !window.location.pathname.endsWith("login.html") || !window.location.pathname.endsWith("signup.html")) {
+        if (!window.location.pathname.endsWith("") || !window.location.pathname.endsWith("login.html") || !window.location.pathname.endsWith("signup.html")) {
             alert("You are not logged in. You will be redirected to the login page.");
             window.location.href = "login.html";
         }
     } else {
-        if (window.location.pathname.endsWith("index.html") || window.location.pathname.endsWith("login.html") || window.location.pathname.endsWith("signup.html")) {
+        if (window.location.pathname.endsWith("") || window.location.pathname.endsWith("login.html") || window.location.pathname.endsWith("signup.html")) {
             window.location.href = "home.html";
         }
     }
