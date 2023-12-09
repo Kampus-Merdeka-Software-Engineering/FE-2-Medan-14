e8925008767c35e3bc9e3052262b81269486d755
Rai Pramana <rai.pramana46@gmail.com>
2023-12-09 11:23:16 +0800

    modified:   js/detailrooms.js

--- js/detailrooms.js
+++ js/detailrooms.js
@@ -1,23 +1,3 @@
-const imgs = document.querySelectorAll(".img-select a");
-const imgBtns = [...imgs];
-let imgId = 1;
-
-imgBtns.forEach((imgItem) => {
-    imgItem.addEventListener("click", (event) => {
-        event.preventDefault();
-        imgId = imgItem.dataset.id;
-        slideImage();
-    });
-});
-
-function slideImage() {
-    const displayWidth = document.querySelector(".img-showcase img:first-child").clientWidth;
-
-    document.querySelector(".img-showcase").style.transform = `translateX(${-(imgId - 1) * displayWidth}px)`;
-}
-
-window.addEventListener("resize", slideImage);
-
 // setting stars rating
 function setRating(rating, totalBooking) {
     if (!rating || !totalBooking || rating < 1 || rating > 5 || totalBooking < 1) {
